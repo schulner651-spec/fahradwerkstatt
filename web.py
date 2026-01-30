27510e9b3253a6ecad9cc49ce4a456de43340758
schulner651-spec <schulner651@gmail.com>
2026-01-30 10:09:45 +0100

    Update web.py

--- web.py
+++ web.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>🚲 Fahrradwerkstatt</h1>
+    <h1>🚲 Fahrradwerkstatt TEST SEITE!</h1>
     <p>Willkommen! Wir melden uns per E-Mail. TEST SEITE!</p>
     """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
